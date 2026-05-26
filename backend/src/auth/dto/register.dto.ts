215bca3b5163db74a2f794f52b18c2aadf2158d6
Svyatoslav334 <sviatoslav.yurkiv-pi2203@kep.nung.edu.ua>
2026-05-26 23:37:44 +0300

    Update register.dto.ts

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,5 +1,5 @@
 import { IsEmail, IsString, MinLength, IsOptional, IsEnum } from 'class-validator';
-import { Role } from 'src/users/users.entity';
+import { Role } from '../../users/users.entity';
 
 export class RegisterDto {
   @IsEmail()
